6f0889851b5251c9e8028db44db9072c692c8ec4
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2020-07-10 02:36:12 +0200

    first submission

--- README.md
+++ README.md
@@ -2,12 +2,21 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+- [Udacity Initial Instructions](#initial-instructions)
+- [Use Completed Project](#use-completed-project)
 
-## Instructions
+## Initial Instructions
 
 The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
-To get started, open `js/app.js` and start building out the app's functionality
+To get started, get `landing-page` project files from [udacity/fend](https://github.com/udacity/fend/tree/refresh-2019) repo, then open `js/app.js` and start building out the app's functionality
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/nanodegrees/).
+
+## Use Completed Project
+
+Download completed project code from this page, then open `index.html` in your browser and start interacting with the page.
+
+Notice how active states of page sections and navigation menu links change while scrolling, and how the window scrolls to the requested section when its navigation link is clicked.
+
+#### This project is submitted as a part of Web Development Professional Nanodegree Program by [Egypt FWD](https://egfwd.com/) initiative
--- js/app.js
+++ js/app.js
@@ -1,57 +1,116 @@
+"use strict";
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
-/**
- * Define Global Variables
- * 
-*/
+/* Define Global Variables */
 
+const sections = document.querySelectorAll("section");
+const navBarList = document.querySelector("#navbar__list");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ */
 
+// function Inspired from https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/#putting-it-all-together
+// check if element or part of it is visible within viewport
+let isInViewport = (elem) => {
+  const box = elem.getBoundingClientRect();
+  return (
+    // element's top border is above the window center
+    box.top <
+      (window.innerHeight || document.documentElement.clientHeight) / 2 &&
+    // either element's top border is below window top border
+    // or element's bottom border is below the window center
+    (box.top >= 0 ||
+      box.bottom >
+        (window.innerHeight || document.documentElement.clientHeight) / 2) &&
+    // element's left and right borders are within window borders
+    box.left >= 0 &&
+    box.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
 
+// function Inspired from https://javascript.info/coordinates#getCoords
+// get document coordinates of the element
+let getCoords = (elem) => {
+  const box = elem.getBoundingClientRect();
+  return {
+    top: box.top + window.pageYOffset,
+    bottom: box.bottom + window.pageYOffset,
+  };
+};
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-
+let buildNav = () => {
+  const frag = document.createDocumentFragment();
+  for (const sect of sections) {
+    const navLink = document.createElement("li");
+    navLink.innerHTML = `<a href="#${sect.id}" class="menu__link">${
+      sect.querySelector("h2").textContent
+    }</a>`;
+    frag.appendChild(navLink);
+  }
+  navBarList.appendChild(frag);
+};
 
 // Add class 'active' to section when near top of viewport
-
-
+let activeSection = () => {
+  for (const sect of sections) {
+    if (sect.nodeName === "SECTION" && isInViewport(sect)) {
+      sect.classList.add("active-section");
+      // add active class to respective nav link
+      document
+        .querySelector(`a[href="#${sect.id}"]`)
+        .classList.add("anchor-viewing");
+    } else if (sect.nodeName === "SECTION" && !isInViewport(sect)) {
+      sect.classList.remove("active-section");
+      // remove active class from respective nav links
+      document
+        .querySelector(`a[href="#${sect.id}"]`)
+        .classList.remove("anchor-viewing");
+    }
+  }
+};
 // Scroll to anchor ID using scrollTO event
-
-
+let smoothScroll = (evt) => {
+  evt.preventDefault();
+  if (evt.target.nodeName === "A") {
+    const elemId = evt.target.getAttribute("href");
+    const elemCoords = getCoords(document.querySelector(`${elemId}`));
+    window.scrollTo({
+      top: elemCoords.top,
+      behavior: "smooth",
+    });
+  }
+};
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
-
+// Build menu
+document.addEventListener("DOMContentLoaded", buildNav);
 // Scroll to section on link click
-
+navBarList.addEventListener("click", smoothScroll);
 // Set sections as active
-
-
+window.addEventListener("scroll", activeSection);
